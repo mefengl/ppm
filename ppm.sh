c055e83d8f7267025a5f426c5782c539a4de9146
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-04-07 10:53:08 +0800

    feat(ppm.sh): add support for force flag to install all packages regardless of whether they are already installed or not feat(ppm.sh): add support for opencommit, jsdoc, @antfu/ni, and kill-port packages fix(ppm.sh): fix yo generator-code package name

--- ppm.sh
+++ ppm.sh
@@ -2,14 +2,21 @@
 
 # Define the packages to install
 packages=(
-"yarn"
-# vercel
-"vercel"
-"turbo"
-# vscode
-"yo generator-code"
-# github
-"@githubnext/github-copilot-cli"
+    # try
+    "opencommit"
+    "jsdoc"
+
+    "@antfu/ni"
+    "yarn"
+    "kill-port"
+    # vercel
+    "vercel"
+    "turbo"
+    # vscode
+    "yo"
+    "generator-code"
+    # github
+    "@githubnext/github-copilot-cli"
 )
 
 # Define color codes for echoing messages
@@ -28,45 +35,53 @@ node_version=$(node -v | cut -c2-)
 IFS='.' read -ra version_parts <<< "$node_version"
 
 if [ ${version_parts[0]} -lt 16 ] || ([ ${version_parts[0]} -eq 16 ] && [ ${version_parts[1]} -lt 4 ]); then
-  echo -e "${RED}Node.js version is below 16.4. Installing packages using npm instead of pnpm.${NC}"
-  installer="npm"
-else
-  # Install pnpm using npm
-  echo -e "${GREEN}Installing pnpm using npm...${NC}"
-  npm -g install pnpm
-  if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Successfully installed pnpm using npm.${NC}"
-    installer="pnpm"
-  else
-    echo -e "${RED}Failed to install pnpm using npm. Falling back to npm for package installation.${NC}"
+    echo -e "${RED}Node.js version is below 16.4. Installing packages using npm instead of pnpm.${NC}"
     installer="npm"
+else
+
+  # Check if pnpm is already installed
+  if npm list -g | grep -q "pnpm"; then
+      echo -e "${GREEN}pnpm is already installed.${NC}"
+      installer="pnpm"
+  else
+      # Install pnpm using npm
+      echo -e "${GREEN}Installing pnpm using npm...${NC}"
+      npm -g install pnpm
+  fi
+  if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Successfully installed pnpm using npm.${NC}"
+      installer="pnpm"
+  else
+      echo -e "${RED}Failed to install pnpm using npm. Falling back to npm for package installation.${NC}"
+      installer="npm"
   fi
 fi
 
 echo $divider
 
-# Check if the -u flag is present, uninstall packages if necessary
-if [ "$1" == "-u" ]; then
-  for package in "${packages[@]}"; do
-    echo -e "${GREEN}Uninstalling ${package} with $installer...${NC}"
-    $installer uninstall -g ${package}
-    if [ $? -eq 0 ]; then
-      echo -e "${GREEN}Successfully uninstalled ${package} with $installer.${NC}"
-    else
-      echo -e "${RED}Failed to uninstall ${package} with $installer.${NC}"
-    fi
-    echo $divider
-  done
-else
-  # Install packages and display a message
-  for package in "${packages[@]}"; do
-    echo -e "${GREEN}Installing ${package} with $installer...${NC}"
-    $installer install -g ${package}
-    if [ $? -eq 0 ]; then
-      echo -e "${GREEN}Successfully installed ${package} with $installer.${NC}"
-    else
-      echo -e "${RED}Failed to install ${package} with $installer.${NC}"
-    fi
-    echo $divider
-  done
+# Check if the -f flag is present, force installation of all packages
+force=false
+if [ "$1" == "-f" ]; then
+    echo -e "${GREEN}Forcing installation of all packages.${NC}"
+    force=true
 fi
+
+# Install or uninstall packages based on the force flag and whether the package is installed
+for package in "${packages[@]}"; do
+    if $force || ! $installer list -g | grep -q $package; then
+        if $force; then
+            echo -e "${GREEN}Force installing ${package} with $installer...${NC}"
+        else
+            echo -e "${GREEN}Installing ${package} with $installer...${NC}"
+        fi
+        $installer install -g ${package}
+        if [ $? -eq 0 ]; then
+            echo -e "${GREEN}Successfully installed ${package} with $installer.${NC}"
+        else
+            echo -e "${RED}Failed to install ${package} with $installer.${NC}"
+        fi
+    else
+        echo -e "${GREEN}${package} is already installed.${NC}"
+    fi
+    echo $divider
+done
